46c7ae264344f16a4e5a39be50ccf3437ef492c3
Wilson LeungZS <wilson_leungzs@163.com>
2019-06-12 14:09:01 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.json({message: 'Response index resource'});
 });
 
 module.exports = router;
