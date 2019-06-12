a7b8821783cb7fbcd964ebdcb445e1d37d5856c0
Wilson LeungZS <wilson_leungzs@163.com>
2019-06-12 14:08:00 +0800

    comment

--- routes/index.js
+++ routes/index.js
@@ -9,3 +9,5 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 // Start server: set DEBUG=PMTBackend & cnpm start
+//提交代码到Github： 1.暂存文件； 2.提交已暂存的文件(add comment); 3.推送
+//同步代码：pull rebase（合并）
