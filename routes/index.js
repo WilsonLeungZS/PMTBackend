c5ce12f8b1c6d6a3b932254197565e35073fd5d7
Wilson LeungZS <wilson_leungzs@163.com>
2019-06-20 19:00:19 +0800

    Update 06-20

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 router.get('/getInfo', function(req, res, next) {
   var rtnResult = [];
   var resJson = {}
-  Reference.findOne({where: {Name: "ProjectName"}}).then(function(reference){
+  Reference.findOne({where: {Name: 'ProjectName'}}).then(function(reference){
     resJson.project_name = reference.Value;
     Task.findAndCountAll().then(function(task){
       resJson.task_count = task.count;
@@ -40,40 +40,83 @@ router.post('/receiveTaskList', function(req, res, next) {
   var taskAssignTeam = req.body.assignment_group;
   var taskAssignTo = req.body.assigned_to;
   var taskCreatedTime = req.body.created;
-  var taskType = req.body.task_type;
+  var taskTypeString = req.body.task_type;
   var session = req.body.session;
   var taskTotalEffort = req.body.task_effort;
-
-  Task.findOrCreate({
-    where: {TaskName: taskNumber}, 
-    defaults: {
-        ParentTaskName: req.body.tParentTaskName,
-        TaskName: req.body.tTaskName,
-        Description: req.body.tDescription,
-        TaskTypeId: req.body.tTaskTypeId,
-        Priority: req.body.tPriority,
-        Status: req.body.tStatus,
-        Creator: req.body.tCreator,
-        Effort: req.body.tEffort,
-        Estimation: req.body.tEstimation,
-        StartDate: req.body.tStartDate,
-        DueDate: req.body.tDueDate
-    }})
-  .spread(function(task, created) {
-    if(created) {
-      console.log("Task created");
-      return res.json(responseMessage(0, task, ''));
-    } else {
-      console.log("Task existed");
-      return res.json(responseMessage(1, null, 'Task existed'));
+  var taskCategorization = [];
+  var taskEffortNum = 0;
+  if(req.body.path != null && req.body.path != ''){
+    taskCategorization = req.body.path;
+  }
+  for(var i=0; i< taskNumber.length; i++){
+    if(taskNumber[i].toUpperCase().startsWith('CG')){
+      taskTypeString[i] = "Change";
+      taskEffortNum = Number(taskTotalEffort[i]);
     }
-});
+    else if(taskNumber[i].toUpperCase().startsWith('PRB')){
+      taskTypeString[i] = 'Problem';
+      taskEffortNum = 24;
+    }
+    else if(taskNumber[i].toUpperCase().startsWith('INCTASK')){
+      taskTypeString[i] = 'ITSR';
+      taskEffortNum = 24;
+    }
+    else if(taskNumber[i].toUpperCase().startsWith('LEAVE')){
+      taskTypeString[i] = 'Leave';
+      taskEffortNum = 0;
+    }
+    else if(taskCategorization != null && taskCategorization.length > 0){
+      if(taskCategorization[i].toUpperCase().startsWith("Service")){
+        taskTypeString[i] = 'Service Request';
+        taskEffortNum = 8;
+      } else {
+        taskTypeString[i] = 'Incident';
+        taskEffortNum = 4;
+      }
+    }
+    else {
+      taskTypeString[i] = 'Sponsor Task';
+    }
+    TaskType.findOne({where: {Name: taskTypeString[i]}}).then(function(taskType) {
+      var taskTypeId = 0;
+      if(taskType != null) {
+        taskTypeId = taskType.Id;
+      } 
+      Task.findOrCreate({
+        where: {TaskName: taskNumber}, 
+        defaults: {
+            ParentTaskName: 'N/A',
+            TaskName: taskNumber[i],
+            Description: taskdesc[i],
+            TaskTypeId: taskTypeId,
+            Status: taskStatus[i],
+            Effort: 0,
+            Estimation: taskEffortNum,
+            StartDate: req.body.tStartDate,
+            createdAt: new Date(taskCreatedTime[i]),
+            AssignTeamId: 0
+        }})
+      .spread(function(task, created) {
+        if(created) {
+          console.log("Task created");
+          return res.json(responseMessage(0, task, ''));
+        } else {
+          console.log("Task existed");
+          return res.json(responseMessage(1, null, 'Task existed'));
+        }
+      });
+    })
+  }
 });
 
 function responseMessage(iStatusCode, iDataArray, iErrorMessage) {
   var resJson = {}; 
   resJson = {status: iStatusCode, data: iDataArray, message: iErrorMessage};
   return resJson;
+}
+
+function mapToTeam(inTeamName){
+
 }
 
 module.exports = router;
