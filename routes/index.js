e297c8ece2eed557e70a69871652ca6f64872b7d
georgeWUZF <654291656@qq.com>
2020-06-10 18:49:58 +0800

    vscode://vscode.github-authentication/did-authenticate?windowId=1&code=4f562c540f6a77af424e&state=83fba7b1-1cdd-4fde-9722-29deb554aee5

--- routes/index.js
+++ routes/index.js
@@ -81,6 +81,7 @@ router.post('/receiveTaskListForSNOW', function(req, res, next) {
       var tTaskType = taskObj.TaskType;
       var tTaskTypeId = 0;
       var taskPoolRef = await getReference('TaskPool', tTaskType);
+      var taskTypeTag = 'One-Off Task';
       if (taskPoolRef != null) {
         if (taskPoolRef.Value != null && taskPoolRef.Value != '') {
           tParentTaskName = taskPoolRef.Value;
@@ -162,7 +163,8 @@ router.post('/receiveTaskListForSNOW', function(req, res, next) {
             TaskLevel: 3, 
             IssueDate: tTaskIssueDate,
             AssigneeId: tAssigneeId,
-            TaskGroupId: tTaskGroupId
+            TaskGroupId: tTaskGroupId,
+            TypeTag: taskTypeTag
         }
       })
       .spread(function(task, created) {
@@ -238,6 +240,7 @@ router.post('/receiveTaskListForTRLS', function(req, res, next) {
       var tTaskType = taskObj.TaskType;
       var tTaskTypeId = 0;
       var taskPoolRef = await getReference('TaskPool', tTaskType);
+      var taskTypeTag = 'One-Off Task';
       Logger.info('Debug 1');
       if (taskPoolRef != null) {
         if (taskPoolRef.Value != null && taskPoolRef.Value != '') {
@@ -282,7 +285,8 @@ router.post('/receiveTaskListForTRLS', function(req, res, next) {
             BizProject: tTaskBizProject,
             BusinessArea: tBusinessArea,
             TaskLevel: 3,
-            IssueDate: tTaskIssueDate
+            IssueDate: tTaskIssueDate,
+            TypeTag: taskTypeTag
         }
       })
       .spread(function(task, created) {
