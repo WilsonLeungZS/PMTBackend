06ff9fea88e2ba07d633b3474c389c338bbf010d
georgeWUZF <654291656@qq.com>
2020-06-26 20:36:51 +0800

    schedule

--- services/taskItem.js
+++ services/taskItem.js
@@ -80,7 +80,7 @@ async function saveTask(req, res, remark) {
             Schedule.findAll({
               attributes: ['JobId'],
               where: { 
-                TaskId: reqTaskName
+                tTaskName: reqTaskName
               },
             }).then(function(sch) {
               var tempJobId = sch[0].JobId;
@@ -243,7 +243,7 @@ async function saveTask(req, res, remark) {
       }).then(function(task) {
         if(task != null) {
           console.log('Task length: ' + task.length);
-          resolve(task.length);
+          resolve(task.length + 1);
         } else {
           resolve(0);
         }
@@ -299,19 +299,13 @@ async function saveTask(req, res, remark) {
     return resJson;
   }
 
-  function createTaskByScheduleJob(TaskId){
-    console.log('Create Regular task ' + TaskId + ' time: ' + new Date());
+  function createTaskByScheduleJob(tTaskName){
+    console.log('Create Regular task ' + tTaskName + ' time: ' + new Date());
     var day = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
     var iParentTask = null;
     Task.findAll({
-      attributes: ['ParentTaskName','TaskName','Description','Status','Creator',
-      'TaskTypeId','Effort','Estimation','IssueDate','TargetCompleteDate','ActualCompleteDate',
-      'BusinessArea','BizProject','TaskLevel','RespLeaderId','AssigneeId','Reference','Scope',
-      'TopConstraint','TopOppName','TopCustomer','TopFacingClient','TopTypeOfWork','TopChanceWinning',
-      'TopSowConfirmation','TopBusinessValue','TopTargetStart','TopTargetEnd','TopPaintPoints',
-      'TopTeamSizing','TopSkill','TopOppsProject','TaskGroupId','TypeTag','DeliverableTag','Detail'],
       where: { 
-        TaskName: TaskId
+        TaskName: tTaskName
       },
     })
     .then(async function(sch) {
@@ -322,9 +316,8 @@ async function saveTask(req, res, remark) {
       for(var i = 0; i < sch.length; i++){
         iParentTask = sch[i].ParentTaskName;
         var subTaskLength = await getSubTaskCount(iParentTask);
-        console.log('George: ' + subTaskLength);
         var TaskName = iParentTask + '-' + subTaskLength;
-        console.log('George TaskName: ' + TaskName);
+        console.log('New TaskName: ' + TaskName + ' which create by Regular task');
         var taskObj = {
           task_parent_name: iParentTask,
           task_name: TaskName,
@@ -366,7 +359,75 @@ async function saveTask(req, res, remark) {
     });
   }
 
+  function getTaskId(tTaskName) {
+    return new Promise((resolve, reject) => {
+      Task.findOne({
+        where: { TaskName: tTaskName }
+      }).then(function(task) {
+        if(task != null) {
+          resolve(task.Id);
+        }
+      });
+    });
+  }
+
+  function getTaskSchedule(tTaskName) {
+    return new Promise((resolve, reject) => {
+      Schedule.findOne({
+        where: { TaskName: tTaskName }
+      }).then(function(task) {
+        if(task != null) {
+          resolve(task.Schedule);
+        }
+      });
+    });
+  }
+
+  function getTaskStatus(tTaskName) {
+    return new Promise((resolve, reject) => {
+      Schedule.findOne({
+        where: { TaskName: tTaskName }
+      }).then(function(task) {
+        if(task != null) {
+          resolve(task.Status);
+        }
+      });
+    });
+  }
+
+  function isSameDay(date1,date2){
+    var oDate1 = new Date(date1);
+    var oDate2 = new Date(date2);
+    if(oDate1 == oDate2){
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+function regularTaskSubTask(tTaskName){
+  Task.findAll({
+    where: {
+      ParentTaskName: tTaskName
+    },
+    order: [
+      ['Id', 'ASC']
+    ]
+  }).then(async function(task) {
+      if(task.length > 0) {
+        for(var i=0;i<task.length;i++){
+          createTaskByScheduleJob(task[i].TaskName);
+          console.log('george: ' + task[i].TaskName);
+        }
+      }
+  })
+}
+
 module.exports = {
   saveTask,
-  createTaskByScheduleJob
+  createTaskByScheduleJob,
+  getTaskId,
+  getTaskSchedule,
+  getTaskStatus,
+  regularTaskSubTask
 }
